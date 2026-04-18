6ab676d50413ce568d45888b9cbd6db4eff86399
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-04-18 19:36:00 +0530

    Initialized frontend UI module

---
+++ frontend/app.py
@@ -0,0 +1,9 @@
+# UI Module (Frontend)
+# Author: Adarsh
+
+# TODO:
+# - Create user interface using Streamlit
+# - Connect with backend
+# - Display emotions and responses
+
+print("UI module working")
